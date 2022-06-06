c172d6d7498843152c531f2c1521648749f92259
Misha Gorshenin <gorshenin.mv@gmail.com>
2022-06-06 23:43:35 +0500

    add ll rotate

--- BinarySearchTree/Library.fs
+++ BinarySearchTree/Library.fs
@@ -35,13 +35,24 @@ module Implementation =
       | Empty -> -1
       | Node node -> node.height
 
+    let ll node =
+      let y =
+        match node.left with
+        | Node node -> node
+        | Empty -> failwith "unexpected type"
+      let a = y.left
+      let b = y.right
+      let c = node.right
+      let x = Node { node with left = b; right = c; height = 1 + max (height b) (height c) }
+      { y with left = a; right = x; height = 1 + max (height a) (height x) }
+
     let balance node =
       let lh = height node.left
       let rh = height node.right
       let diff = abs (lh - rh)
       match diff with
       | diff when diff < 2 -> node
-      | _ -> failwith "balance required"
+      | _ -> ll node
 
     let rec insertInternal tree value =
       match tree with
@@ -140,7 +151,19 @@ module Tests =
     let tree = Node { leaf with value = 42 }
     (fun () -> insert 42 tree |> ignore) |> should throw typeof<System.Exception>
 
-  [<TestCase([| 42; 10; 60; 50; 70; 90 |])>]
-  [<TestCase([| 42; 60; 70 |])>]
-  let ``if diff more than 2 balance required`` valuesToInsert =
-    (fun () -> fillTree valuesToInsert |> ignore) |> should (throwWithMessage "balance required") typeof<System.Exception>
+  [<Test>]
+  let ``ll check`` () =
+    let tree = fillTree [| 10; 5; 2 |]
+    let expectedTree = Node {
+      value = 5
+      left = Node { leaf with value = 2 }
+      right = Node {leaf with value = 10}
+      height = 1
+    }
+    tree |> should equal expectedTree
+
+  [<Test>]
+  let ``how immutability works`` () =
+    let tree = fillTree [| 42; 10; 60; 50 |]
+    let copy = tree
+    isSameTrees tree copy |> should equal true
